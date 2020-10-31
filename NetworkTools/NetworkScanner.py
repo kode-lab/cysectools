d5ec09db736ab08a959c23d49fc080ad01db3d62
kode-lab <kode-lab@users.noreply.github.com>
2020-10-30 19:15:09 -0600

    NetworkScanner is machine interactable

--- NetworkTools/NetworkScanner.py
+++ NetworkTools/NetworkScanner.py
@@ -1,34 +1,41 @@
 # Kode Blue, 10/29/2020
-# Mostly followed this tutorial: https://www.thepythoncode.com/article/building-network-scanner-using-scapy
+# Tool for scanning for hosts on a network
 # Cheers!
+# NetworkScanner.py
 
-# TODO include info for CIDR notation
+# TODO what the heck kind of datatype is "clients"?
 
 from scapy.all import ARP, Ether, srp  # import tools from scapy (no need to reinvent the wheel :)
 
-target_ip = input("Which IP to scan? Default 192.164.1.1/24 : ")
-if target_ip == "":
-    target_ip = "192.164.1.1/24"      # IP address for the destination. 
+def scan_network(target):
+    """
+    Returns data of clients on the scanned network
+    """
+    arp = ARP(pdst=target)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    
+    packet = ether/arp
+    
+    result = srp (packet, timeout=3)[0]
+    
+    clients=[]
+    
+    for sent, received in result:
+        clients.append({"ip": received.psrc, 'mac': received.hwsrc})
+    
+    return clients
 
-arp = ARP(pdst=target_ip)   # Create the ARP packet
+def main(target):
+    clients = scan_network(target)
+    
+    print("Available Devices on the Network: ")
+    print("IP" + " "*18 + "MAC")
+    
+    for client in clients:
+        print("{:16}      {}".format(client['ip'], client['mac']))
 
-ether = Ether(dst="ff:ff:ff:ff:ff:ff")     # Create Ether broadcast packet (MAC here indicates broadcasting)
-
-packet = ether/arp     # Stack the two packets
-
-result = srp(packet, timeout=3)[0]     # send the packets (srp()) over Layer 2, timeout 3 so the script dont get stuck
-
-clients = []
-
-for sent, received in result:
-    # For each response, append ip and mac address to the list 'clients'
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
-
-# PRINT the resulting list of clients
-print("Available Devices on the network:")
-print("IP"+" "*18+"MAC")
-
-for client in clients:
-    print("{:16}      {}".format(client['ip'], client['mac']))
-
-
+if __name__ == "__main__":
+    target_ip = input("Which IP to scan? Default 192.168.1.1/24 : ")
+    if target_ip == "":
+        target_ip = "192.168.1.1/24"      # IP address for the destination. 
+    main(target_ip)
